c8992854f794b48f00c9e9e6b268d322e4bacde9
ashikur rahman <ashikorrahman8900@gmail.com>
2026-04-15 23:04:17 +0600

    added sort fuctionality

--- src/components/FriendDetailsPage/CheckInBtn.jsx
+++ src/components/FriendDetailsPage/CheckInBtn.jsx
@@ -1,28 +1,34 @@
-"use client"
+"use client";
 import { TimelineContext } from "@/context/timelineContext";
 import React, { useContext } from "react";
 import { toast } from "react-toastify";
 
-const CheckInBtn = ({item, friend}) => {
-  const {timeline, setTimeline} = useContext(TimelineContext);
+const CheckInBtn = ({ item, friend }) => {
+  const { timeline, setTimeline } = useContext(TimelineContext);
 
-  const handleTimeline = (text)=> {
+  const handleTimeline = (text) => {
     const timelineObj = {
-      id: friend.id,
+      time: Date.now(),
       date: new Date().toLocaleDateString("en-us", {
-        year: 'numeric', 
-        month: 'long', 
-        day: 'numeric'
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "2-digit",
+        hour12: true,
       }),
       text: text,
-      name: friend.name
-    }
-    setTimeline([...timeline, timelineObj])
-    toast.success(`${text} with ${friend.name}`)
-  }
-    console.log(timeline);
+      name: friend.name,
+    };
+    setTimeline([...timeline, timelineObj]);
+    toast.success(`${text} with ${friend.name}`);
+  };
+
   return (
-    <button onClick={()=> handleTimeline(item.label)} className="btn w-full h-auto flex flex-col p-4 sm:p-6 bg-gray-100 shadow-sm rounded-2xl">
+    <button
+      onClick={() => handleTimeline(item.label)}
+      className="btn w-full h-auto flex flex-col p-4 sm:p-6 bg-gray-100 shadow-sm rounded-2xl"
+    >
       <span className="text-2xl text-gray-500">{item.icon}</span>
 
       <p className="text-gray-500 font-medium">{item.label}</p>
--- src/components/timelinePage/AllTimelineEntry.jsx
+++ src/components/timelinePage/AllTimelineEntry.jsx
@@ -6,34 +6,74 @@ import TimelineCard from "../ui/TimelineCard";
 const AllTimelineEntry = () => {
   const { timeline } = useContext(TimelineContext);
   const [filterValue, setFilterValue] = useState("");
+  const [sortValue, setSortValue] = useState("");
 
-  const handleFilter = (e)=>{
+  const handleFilter = (e) => {
     const value = e.target.value;
     setFilterValue(value);
-  }
+  };
+  const handleSort = (e) => {
+    const value = e.target.value;
+    setSortValue(value);
+  };
 
-  const filteredArray = ()=>{
-    const filteredTimeline = timeline;
-    if(filterValue === "Call" || filterValue === "Text" || filterValue === "Video"){
-        return filteredTimeline.filter(item=> item.text === filterValue);
+  const filteredArray = () => {
+    const filteredTimeline = [...timeline];
+    if (
+      filterValue === "Call" ||
+      filterValue === "Text" ||
+      filterValue === "Video"
+    ) {
+      return filteredTimeline.filter((item) => item.text === filterValue);
     }
     return filteredTimeline;
-  }
+  };
+
+  const sortedArray = () => {
+    const sortedTimeline = filteredArray();
+
+    if (sortValue === "newest") {
+      return sortedTimeline.sort((a, b) => b.time - a.time);
+    } else if (sortValue === "oldest") {
+      return sortedTimeline.sort((a, b) => a.time - b.time);
+    }
+
+    return sortedTimeline;
+  };
+  console.log(sortValue);
   return (
     <>
-      <div className="max-w-70 mt-5">
-        <select onChange={handleFilter} className="w-full pl-4 pr-10 py-2 text-sm text-gray-400 bg-white border border-gray-200 rounded-lg shadow-sm cursor-pointer">
-          <option>Filter timeline</option>
-          <option>Call</option>
-          <option>Text</option>
-          <option>Video</option>
-        </select>
+      <div className="flex justify-start items-center gap-2 flex-wrap">
+        <div className="max-w-70 mt-5">
+          <select defaultValue=""
+            onChange={handleFilter}
+            className="w-full pl-4 pr-20 py-2 text-sm text-gray-600 bg-white border border-gray-200 rounded-lg shadow-sm cursor-pointer"
+          >
+            <option disabled hidden>Filter timeline</option>
+            <option value="All">All</option>
+            <option value="Call">Call</option>
+            <option value="Text">Text</option>
+            <option value="Video">Video</option>
+          </select>
+        </div>
+        <div className="max-w-70 mt-5">
+          <select defaultValue=""
+            onChange={handleSort}
+            className="w-full pl-4 pr-10 py-2 text-sm text-gray-600 bg-white border border-gray-200 rounded-lg shadow-sm cursor-pointer"
+          >
+            <option disabled hidden>Sort By</option>
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
+          </select>
+        </div>
       </div>
       <div className="mt-10">
-        {filteredArray().length === 0 ? (
-          <p className="text-center bg-white p-7 shadow-sm rounded-xl  text-2xl font-semibold text-gray-400">Timeline is empty</p>
+        {sortedArray().length === 0 ? (
+          <p className="text-center bg-white p-7 shadow-sm rounded-xl  text-2xl font-semibold text-gray-400">
+            Timeline is empty
+          </p>
         ) : (
-          filteredArray().map((item, ind) => (
+          sortedArray().map((item, ind) => (
             <TimelineCard key={ind} item={item}></TimelineCard>
           ))
         )}

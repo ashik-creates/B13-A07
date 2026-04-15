bcefa855fb9b7992af3a949beb350cf2a735ff7f
ashikur rahman <ashikorrahman8900@gmail.com>
2026-04-16 01:56:23 +0600

    added search functionality

--- src/components/timelinePage/AllTimelineEntry.jsx
+++ src/components/timelinePage/AllTimelineEntry.jsx
@@ -6,6 +6,8 @@ import TimelineCard from "../ui/TimelineCard";
 const AllTimelineEntry = () => {
   const { timeline } = useContext(TimelineContext);
   const [filterValue, setFilterValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+
   const [sortValue, setSortValue] = useState("");
 
   const handleFilter = (e) => {
@@ -17,63 +19,84 @@ const AllTimelineEntry = () => {
     setSortValue(value);
   };
 
-  const filteredArray = () => {
-    const filteredTimeline = [...timeline];
-    if (
-      filterValue === "Call" ||
-      filterValue === "Text" ||
-      filterValue === "Video"
-    ) {
-      return filteredTimeline.filter((item) => item.text === filterValue);
+  const filteredTimeline = timeline.filter((item) => {
+    const searchLower = searchValue.toLocaleLowerCase();
+    const searchedArray =
+      item.text.toLowerCase().includes(searchLower) ||
+      item.name.toLowerCase().includes(searchLower);
+    if (!searchedArray) {
+      return false;
+    } else {
+      if (
+        filterValue === "Call" ||
+        filterValue === "Text" ||
+        filterValue === "Video"
+      ) {
+        return item.text === filterValue;
+      } else {
+        return true;
+      }
     }
-    return filteredTimeline;
-  };
+  });
 
-  const sortedArray = () => {
-    const sortedTimeline = filteredArray();
-
-    if (sortValue === "newest") {
-      return sortedTimeline.sort((a, b) => b.time - a.time);
-    } else if (sortValue === "oldest") {
-      return sortedTimeline.sort((a, b) => a.time - b.time);
+  const displaySortedOrFilteredTimeline = [...filteredTimeline].sort((a,b)=>{
+    if (sortValue === "newest"){
+      return b.time - a.time;
     }
+    else if(sortValue === "oldest"){
+      return a.time - b.time;
+    }
+    return 0;
+  })
 
-    return sortedTimeline;
-  };
-  console.log(sortValue);
   return (
     <>
-      <div className="flex justify-start items-center gap-2 flex-wrap">
-        <div className="max-w-70 mt-5">
-          <select defaultValue=""
+      <div className="flex justify-start items-center gap-4 flex-wrap mt-5">
+        <div className="max-w-70 ">
+          <select
+            defaultValue=""
             onChange={handleFilter}
             className="w-full pl-4 pr-20 py-2 text-sm text-gray-600 bg-white border border-gray-200 rounded-lg shadow-sm cursor-pointer"
           >
-            <option disabled hidden>Filter timeline</option>
+            <option value="" disabled hidden>Filter timeline</option>
             <option value="All">All</option>
             <option value="Call">Call</option>
             <option value="Text">Text</option>
             <option value="Video">Video</option>
           </select>
         </div>
-        <div className="max-w-70 mt-5">
-          <select defaultValue=""
+        <div className="max-w-70 ">
+          <select
+            defaultValue=""
             onChange={handleSort}
             className="w-full pl-4 pr-10 py-2 text-sm text-gray-600 bg-white border border-gray-200 rounded-lg shadow-sm cursor-pointer"
           >
-            <option disabled hidden>Sort By</option>
+            <option value="" disabled hidden>
+              Sort By
+            </option>
             <option value="newest">Newest</option>
             <option value="oldest">Oldest</option>
           </select>
         </div>
+        <div className="flex justify-center items-center">
+          <input
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            type="text"
+            placeholder="Search friends..."
+            className="w-full px-4 py-1.5 border border-gray-300 
+            border-r-0
+            rounded-lg rounded-r-0"
+          />
+        </div>
       </div>
       <div className="mt-10">
-        {sortedArray().length === 0 ? (
+        {displaySortedOrFilteredTimeline.length === 0 ? (
           <p className="text-center bg-white p-7 shadow-sm rounded-xl  text-2xl font-semibold text-gray-400">
             Timeline is empty
           </p>
         ) : (
-          sortedArray().map((item, ind) => (
+          displaySortedOrFilteredTimeline.map((item, ind) => (
             <TimelineCard key={ind} item={item}></TimelineCard>
           ))
         )}
